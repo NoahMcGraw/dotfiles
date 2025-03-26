f99fe584b163406ed531c874c143ff9e4c136da2
NoahMcGraw <nmcgr1996@hotmail.com>
2025-03-26 12:12:50 -0500

    feature: step flags

--- .git-shortcuts.bash
+++ .git-shortcuts.bash
@@ -8,20 +8,53 @@
 __git_chain() {
     local cmd=""
     local input=${1#g}  # Remove the leading 'g'
-    local msg=$2
+    shift
+    local args=("$@")
+    local current_flag=""
+    local command_args=()
+    
+    # Parse flags and their arguments
+    declare -A flag_args
+    for arg in "${args[@]}"; do
+        if [[ $arg == -* ]]; then
+            current_flag="${arg:1}"  # Remove the leading dash
+        else
+            if [ -n "$current_flag" ]; then
+                flag_args[$current_flag]="$arg"
+                current_flag=""
+            fi
+        fi
+    done
+
     while [ -n "$input" ]; do
         case "${input:0:2}" in
-            co) cmd+="git commit -m \"$msg\" && "; input="${input:2}";;
+            co) 
+                local msg=${flag_args[co]:-""}
+                cmd+="git commit -m \"$msg\" && "
+                input="${input:2}"
+                ;;
             pl) cmd+="git pull && "; input="${input:2}";;
             ps) cmd+="git push && "; input="${input:2}";;
-            ch) cmd+="git checkout && "; input="${input:2}";;
+            ch) 
+                local branch=${flag_args[ch]:-""}
+                cmd+="git checkout $branch && "
+                input="${input:2}"
+                ;;
             st) cmd+="git stash && "; input="${input:2}";;
             *)
                 case "${input:0:1}" in
                     a) cmd+="git add -A && "; input="${input:1}";;
                     d) cmd+="git diff && "; input="${input:1}";;
-                    r) cmd+="git rebase && "; input="${input:1}";;
-                    m) cmd+="git merge && "; input="${input:1}";;
+                    r) 
+                        local branch=${flag_args[r]:-""}
+                        cmd+="git rebase $branch && "
+                        input="${input:1}"
+                        ;;
+                    m) 
+                        local branch=${flag_args[m]:-""}
+                        cmd+="git merge $branch && "
+                        input="${input:1}"
+                        ;;
                     b) cmd+="git branch && "; input="${input:1}";;
                     s) cmd+="git status && "; input="${input:1}";;
                     *) input="${input:1}";;
