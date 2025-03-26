caf84cfefe35a0df17e1f373b6aaffa0d9fb1fea
NoahMcGraw <nmcgr1996@hotmail.com>
2025-03-26 10:36:42 -0500

    feature: adding dynamic command chaining

--- .git-shortcuts.bash
+++ .git-shortcuts.bash
@@ -4,13 +4,44 @@
 #
 # A collection of git shortcuts for bash.
 
-alias gs="git status"
-alias gch="git checkout"
-alias ga="git add -A"
-alias gaco="git add -A && git commit -m"
-alias gco="git commit -m"
-alias gpl="git pull"
-alias gps="git push"
-alias gd="git diff"
-alias gr="git rebase"
-alias gm="git merge"
+# Basic git shortcuts
+# alias gs="git status"
+# alias gch="git checkout"
+# alias ga="git add -A"
+# alias gaco="git add -A && git commit -m"
+# alias gco="git commit -m"
+# alias gpl="git pull"
+# alias gps="git push"
+# alias gd="git diff"
+# alias gr="git rebase"
+# alias gm="git merge"
+# alias gb="git branch"
+# alias gsts="git stash"
+
+# Function to chain git commands
+g() {
+    if [ $# -eq 0 ]; then
+        git status  # Default behavior if no arguments
+        return
+    fi
+
+    local cmd=""
+    for part in $(echo "$1" | grep -o '.'); do
+        case $part in
+            s) cmd="$cmd git status &&";;
+            a) cmd="$cmd git add -A &&";;
+            c) cmd="$cmd git commit -m \"$2\" &&";;
+            ">") cmd="$cmd git push &&";;
+            "<") cmd="$cmd git pull &&";;
+            "-") cmd="$cmd git stash &&";;
+            "~") cmd="$cmd git checkout &&";;
+            d) cmd="$cmd git diff &&";;
+            r) cmd="$cmd git rebase &&";;
+            m) cmd="$cmd git merge &&";;
+            b) cmd="$cmd git branch &&";;
+            *) echo "Unknown command: $part"; return 1;;
+        esac
+    done
+    cmd=${cmd%" &&"}  # Remove the trailing &&
+    eval $cmd
+}
